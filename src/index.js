a39ddfa6d3efc532d2e5dddfcc3d4491695a0e80
Sithu-BinaryLab <sithulwin.binarylab@gmail.com>
2023-09-27 09:49:46 +0630

    react-redux prototype

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// Redux Import
+import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
+ 
+// Store
+import configureStore from './store/configureStore';
+ 
+const store = configureStore();
+ 
+ReactDOM.render(
+  // Provider
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+ 
+reportWebVitals()
